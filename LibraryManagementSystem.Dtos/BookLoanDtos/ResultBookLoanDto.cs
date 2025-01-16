0bcee40141a0dfd20b42f22e13d0fd0fbfb44c4c
boratln <boratulan123@gmail.com>
2025-01-16 18:26:26 +0300

    Book Loan Dtos

---
+++ LibraryManagementSystem.Dtos/BookLoanDtos/ResultBookLoanDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Dtos.BookLoanDtos
+{
+    public class ResultBookLoanDto
+    {
+        public int BookLoanId { get; set; }
+        public int MemberId { get; set; }
+    }
+}
---
+++ LibraryManagementSystem.Dtos/BookLoanDtos/UpdateBookLoanDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Dtos.BookLoanDtos
+{
+    public class UpdateBookLoanDto
+    {
+        public int BookLoanId { get; set; }
+        public int MemberId { get; set; }
+    }
+}

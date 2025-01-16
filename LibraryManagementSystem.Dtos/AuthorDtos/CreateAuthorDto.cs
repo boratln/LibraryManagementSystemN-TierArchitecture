013e7c536832e47d098eff0be89fe01f3f883765
boratln <boratulan123@gmail.com>
2025-01-16 18:25:52 +0300

    Author Dtos

---
+++ LibraryManagementSystem.Dtos/AuthorDtos/CreateAuthorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Dtos.AuthorDtos
+{
+    public class CreateAuthorDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string ShortDesc { get; set; }
+        public string AuthorImageBase64 { get; set; }
+    }
+}

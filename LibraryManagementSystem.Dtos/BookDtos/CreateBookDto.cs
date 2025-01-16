db832f905e0c3da901112f2af9f148db73f4564a
boratln <boratulan123@gmail.com>
2025-01-16 18:26:09 +0300

    Book Dtos

---
+++ LibraryManagementSystem.Dtos/BookDtos/CreateBookDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Dtos.BookDtos
+{
+    public class CreateBookDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int PageCount { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int StockCount { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
---
+++ LibraryManagementSystem.Dtos/BookDtos/ResultBookDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Dtos.BookDtos
+{
+    public class ResultBookDto
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int PageCount { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int StockCount { get; set; }
+        public int AuthorId { get; set; }
+    }
+}

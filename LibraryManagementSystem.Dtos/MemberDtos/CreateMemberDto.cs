47c9b8bdb63a0049321894cf4de7ae709bcc2e1e
boratln <boratulan123@gmail.com>
2025-01-16 18:26:34 +0300

    Member Dtos

---
+++ LibraryManagementSystem.Dtos/MemberDtos/CreateMemberDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Dtos.MemberDtos
+{
+    public class CreateMemberDto
+    {
+        public string MemberName { get; set; }
+        public string MemberSurname { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ LibraryManagementSystem.Dtos/MemberDtos/UpdateMemberDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Dtos.MemberDtos
+{
+    public class UpdateMemberDto
+    {
+        public int MemberId { get; set; }
+        public string MemberName { get; set; }
+        public string MemberSurname { get; set; }
+        public int Age { get; set; }
+    }
+}

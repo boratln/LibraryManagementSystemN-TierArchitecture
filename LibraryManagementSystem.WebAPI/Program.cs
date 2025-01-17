7b8a0d81101d2e017394623e114d11501cb30e96
boratln <boratulan123@gmail.com>
2025-01-17 16:57:44 +0300

    Dependency Injection

--- LibraryManagementSystem.WebAPI/Program.cs
+++ LibraryManagementSystem.WebAPI/Program.cs
@@ -1,8 +1,21 @@
+using LibraryManagementSystem.Application.Interfaces;
+using LibraryManagementSystem.Application.Services;
+using LibraryManagementSystem.Persistence.Context;
+using LibraryManagementSystem.Persistence.Repositories;
 using System.Reflection;
-
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<LibraryManagementContext>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-// Add services to the container.
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IBookLoanRepository, BookLoanRepository>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+
+builder.Services.AddScoped<IBookService,BookService>();
+builder.Services.AddScoped<IBookLoanService, BookLoanService>();
+builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<IAuthorService,AuthorService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

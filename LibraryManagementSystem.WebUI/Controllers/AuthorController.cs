09ae14cd7eba2f03c0606480c5ac5d07813934cd
boratln <boratulan123@gmail.com>
2025-01-17 16:58:40 +0300

    Web UI Controllers

---
+++ LibraryManagementSystem.WebUI/Controllers/AuthorController.cs
@@ -0,0 +1,30 @@
+﻿using LibraryManagementSystem.WebUI.Dtos.AuthorDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.WebSockets;
+
+namespace LibraryManagementSystem.WebUI.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AuthorController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async  Task<IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7019/api/Author");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData=await response.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ LibraryManagementSystem.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult UILayout()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

c61622e89cea83ee6ded52cbe469e5f0fefaf142
boratln <boratulan123@gmail.com>
2025-01-17 16:54:04 +0300

    Update IAuthorRepository.cs

--- LibraryManagementSystem.Application/Interfaces/IAuthorRepository.cs
+++ LibraryManagementSystem.Application/Interfaces/IAuthorRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagementSystem.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Application.Interfaces
 {
-    public interface IAuthorRepository
+    public interface IAuthorRepository:IRepository<Author>
     {
+        Author SearchAuthor(string word);
     }
 }

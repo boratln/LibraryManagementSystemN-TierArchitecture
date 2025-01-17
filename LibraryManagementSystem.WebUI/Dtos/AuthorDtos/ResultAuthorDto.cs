90adae39c33dcf27221100b6937a678eb5d8f8f1
boratln <boratulan123@gmail.com>
2025-01-17 16:58:50 +0300

    Web UI Result Author Dto

---
+++ LibraryManagementSystem.WebUI/Dtos/AuthorDtos/ResultAuthorDto.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagementSystem.WebUI.Dtos.AuthorDtos
+{
+    public class ResultAuthorDto
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string ShortDesc { get; set; }
+        public string AuthorImageBase64 { get; set; }
+    }
+}
